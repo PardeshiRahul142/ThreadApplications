15c3e3160b46ba19cb13e4bbc6d2e964043f2dfd
Pardeshi Rahul <rahulpardeshi949@gmail.com>
2025-11-05 16:05:31 +0530

    Second pro

---
+++ THAPP.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class J extends Thread
+{   
+  public void run()
+   {
+      try
+      { for(int i=0;i<5;i++) 
+        {  System.out.println("I am j");
+           Thread.sleep(10000);
+        }
+      }
+
+   catch(Exception e)
+   { System.out.println(e);
+   }
+  }
+}//class
+class K extends Thread
+{ public void run()
+  { 
+     try
+      { for(int i=0;i<50;i++)
+        { System.out.println("I am k...");
+          Thread.sleep(1000);
+        }
+      }
+  catch(Exception e)
+  { System.out.println(e);
+  }
+ }
+}//class
+public class THAPP
+{ public static void main(String args[]) 
+  {  J j=new J();
+      j.start();
+
+    K k=new K();
+      k.start();
+  }
+}//class
